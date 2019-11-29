fd35725a7f3ac671be1e64deb48484c3262625c5
Alex Miro <alexmirosh37@gmail.com>
2019-11-29 13:09:19 +0300

    added placing persons on canvas

--- frontend/octoquarium.js
+++ frontend/octoquarium.js
@@ -1,23 +1,28 @@
-const radius = 40;
-const interestRadius = 30;
-const interestDistance = 100
-const area = 2 * (radius + interestRadius + interestDistance + 20)
+const personRadius = 20;
+const interestRadius = 10;
+const interestDistance = 50;
+const area = 2 * (personRadius + interestRadius + interestDistance + 20);
+const circleNum = 3;
+const circleDistance = 150;
+
+let cnv = document.getElementById("octoquarium")
+let ctx = cnv.getContext("2d");
 
 class Person {
-    constructor (interests){
+    constructor (interests, x, y){
         // this.imgage = image;
         this.interests = interests;
         this.pos = {
-            x : 500,
-            y : 500
-        }
+            x : x,
+            y : y
+        };
         
-        this.startAngle = 2 * Math.PI * Math.random()
+        this.startAngle = 2 * Math.PI * Math.random();
     }
 
     draw(ctx) {
         ctx.beginPath();
-        ctx.arc(this.pos.x,this.pos.y, radius , 0, 2 * Math.PI, false);
+        ctx.arc(this.pos.x,this.pos.y, personRadius, 0, 2 * Math.PI, false);
         ctx.lineWidth = 5;
         ctx.strokeStyle = '#003300';
         ctx.stroke();
@@ -40,8 +45,6 @@ class Person {
             ctx.fillText(this.interests[i], interestPos.x - this.interests[i].length * 0.5 * 5, interestPos.y);
             ctx.fill();
         }
-
-        
     }
 
     update(){
@@ -50,19 +53,41 @@ class Person {
 }
 
 
-let cnv = document.getElementById("octoquarium")
-let ctx = cnv.getContext("2d");
-let persons = []
-for(let i=0; i<10; i++){
-    let person = new Person(["Harry Potter", "Machine Learning", "Fitness"])
-    persons.push(person)
+let persons = [];
+let numOfPersons = 10; 
+
+let circleStepAngle = 2 * Math.PI / circleNum
+
+
+for(let i=0; i<=numOfPersons; i++){
+    let pos;
+    if (i == 0){
+        pos = {
+            x : 400,
+            y : 400
+        };
+
+    } else {
+        radius = (Math.floor(i / (circleNum + 1)) + 1) * circleDistance;
+        // console.log(radius);
+        pos = {
+            x : 400 + radius * Math.cos(circleStepAngle * (i - 1 + 0.5 * ((Math.floor(i / (circleNum + 1))) % 2))),
+            y : 400 + radius * Math.sin(circleStepAngle * (i - 1 + 0.5 * ((Math.floor(i / (circleNum + 1))) % 2)))
+        };
+        console.log(((Math.floor(i / circleNum)) % 2));
+    }
+    
+    let person = new Person(["Harry Potter", "Machine Learning", "Fitness"], pos.x, pos.y)
+
+    
+    persons.push(person);
 }
 
 function update(progress) {
     // Update the state of the world for the elapsed time since last render
 
     for(person of persons){
-        person.update()
+        person.update();
     }
 }
   
@@ -73,19 +98,20 @@ function draw() {
 
     ctx.clearRect(0, 0, cnv.width, cnv.height);
     for(person of persons){
-        person.draw(ctx)
+        person.draw(ctx);
     }
 }
 
 function loop(timestamp) {
-let progress = timestamp - lastRender
+    let progress = timestamp - lastRender;
 
-update(progress)
-draw()
+    update(progress);
+    draw();
 
-lastRender = timestamp
-window.requestAnimationFrame(loop)
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop);
 }
-let lastRender = 0
-window.requestAnimationFrame(loop)
+
+let lastRender = 0;
+window.requestAnimationFrame(loop);
 
