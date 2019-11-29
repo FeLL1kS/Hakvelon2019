10af627a0f80221ab6947495d7315d8feb10b7cd
Alex Miro <alexmirosh37@gmail.com>
2019-11-29 21:55:40 +0300

    working octoquarium with zoom and scrolling

--- frontend/octoquarium.js
+++ frontend/octoquarium.js
@@ -7,7 +7,7 @@ const maxCirlceDistance = 30;
 
 let cnv = document.getElementById("octoquarium")
 let ctx = cnv.getContext("2d");
-
+trackTransforms(ctx);
 
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
@@ -161,7 +161,6 @@ for(let i=0; i<numOfPersons; i++){
         };
 
     } else {
-        // radius = (Math.floor(i  / (circleNum + 1) + 1)) * circleDistance
         radius = radiusCounter * circleDistance
         pos = {
             x : window.innerWidth / 2 + radius * Math.cos(circleStepAngle * (i - 1) + randomStartAngle),
@@ -192,12 +191,17 @@ function update(progress) {
     }
 }
 
+
 function draw() {
     // Draw the state of the world
     // ctx.canvas.width  = window.innerWidth;
     // ctx.canvas.height = window.innerHeight;
 
-    ctx.clearRect(0, 0, cnv.width, cnv.height);
+    // Clear the entire canvas
+    var p1 = ctx.transformedPoint(0,0);
+    var p2 = ctx.transformedPoint(cnv.width,cnv.height);
+    ctx.clearRect(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
+
     for(person of persons){
         person.draw(ctx);
     }
@@ -215,3 +219,103 @@ function loop(timestamp) {
 
 let lastRender = 0;
 window.requestAnimationFrame(loop);
+
+
+var lastX=cnv.width/2, lastY=cnv.height/2;
+var dragStart,dragged;
+cnv.addEventListener('mousedown',function(evt){
+    document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+    lastX = evt.offsetX || (evt.pageX - cnv.offsetLeft);
+    lastY = evt.offsetY || (evt.pageY - cnv.offsetTop);
+    dragStart = ctx.transformedPoint(lastX,lastY);
+    dragged = false;
+},false);
+cnv.addEventListener('mousemove',function(evt){
+    lastX = evt.offsetX || (evt.pageX - cnv.offsetLeft);
+    lastY = evt.offsetY || (evt.pageY - cnv.offsetTop);
+    dragged = true;
+    if (dragStart){
+        var pt = ctx.transformedPoint(lastX,lastY);
+        ctx.translate(pt.x-dragStart.x,pt.y-dragStart.y);
+        draw();
+    }
+},false);
+cnv.addEventListener('mouseup',function(evt){
+    dragStart = null;
+    if (!dragged) zoom(evt.shiftKey ? -1 : 1 );
+},false);
+
+var scaleFactor = 1.1;
+var zoom = function(clicks){
+    var pt = ctx.transformedPoint(lastX,lastY);
+    ctx.translate(pt.x,pt.y);
+    var factor = Math.pow(scaleFactor,clicks);
+    ctx.scale(factor,factor);
+    ctx.translate(-pt.x,-pt.y);
+    draw();
+}
+
+var handleScroll = function(evt){
+    var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
+    if (delta) zoom(delta);
+    return evt.preventDefault() && false;
+};
+cnv.addEventListener('DOMMouseScroll',handleScroll,false);
+cnv.addEventListener('mousewheel',handleScroll,false);
+
+
+function trackTransforms(ctx){
+    var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+    var xform = svg.createSVGMatrix();
+    ctx.getTransform = function(){ return xform; };
+    
+    var savedTransforms = [];
+    var save = ctx.save;
+    ctx.save = function(){
+        savedTransforms.push(xform.translate(0,0));
+        return save.call(ctx);
+    };
+    var restore = ctx.restore;
+    ctx.restore = function(){
+        xform = savedTransforms.pop();
+        return restore.call(ctx);
+    };
+
+    var scale = ctx.scale;
+    ctx.scale = function(sx,sy){
+        xform = xform.scaleNonUniform(sx,sy);
+        return scale.call(ctx,sx,sy);
+    };
+    var rotate = ctx.rotate;
+    ctx.rotate = function(radians){
+        xform = xform.rotate(radians*180/Math.PI);
+        return rotate.call(ctx,radians);
+    };
+    var translate = ctx.translate;
+    ctx.translate = function(dx,dy){
+        xform = xform.translate(dx,dy);
+        return translate.call(ctx,dx,dy);
+    };
+    var transform = ctx.transform;
+    ctx.transform = function(a,b,c,d,e,f){
+        var m2 = svg.createSVGMatrix();
+        m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
+        xform = xform.multiply(m2);
+        return transform.call(ctx,a,b,c,d,e,f);
+    };
+    var setTransform = ctx.setTransform;
+    ctx.setTransform = function(a,b,c,d,e,f){
+        xform.a = a;
+        xform.b = b;
+        xform.c = c;
+        xform.d = d;
+        xform.e = e;
+        xform.f = f;
+        return setTransform.call(ctx,a,b,c,d,e,f);
+    };
+    var pt  = svg.createSVGPoint();
+    ctx.transformedPoint = function(x,y){
+        pt.x=x; pt.y=y;
+        return pt.matrixTransform(xform.inverse());
+    }
+}
